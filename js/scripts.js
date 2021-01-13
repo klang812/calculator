665add3731964513a22cd13cb223a2acaf222ea7
Kerry Lang <klang812@gmail.com>
2021-01-13 15:44:46 -0800

    add addition function

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,22 @@ const add=function(number1, number2) {
 }
 
 const subtract=function(number1, number2) {
-	return number1 + number2;
+	return number1 - number2;
 }
 
 const multiply=function(number1, number2) {
-	return number1 + number2;
+	return number1 * number2;
 }
 
 const divide=function(number1, number2) {
-	return number1 + number2;
+	return number1 / number2;
 }
 
-
-const number1=parseInt(prompt("Enter a number:"));
-const number2=parseInt(prompt("Enter another number:"));
-
-alert(add(number1,number2));
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+	event.preventDefault();
+	const number1=parseInt($("#add1").val());
+	const number2=parseInt($("#add2").val());
+	alert(add(number1,number2));
+	});
+});
