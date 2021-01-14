972da8cd3d37e448306a97b5598c949157161337
Kerry Lang <klang812@gmail.com>
2021-01-13 19:45:04 -0800

    move subtract total to below the subract button

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,13 @@ $(document).ready(function() {
 	$("#output").text(result);
 	});
 });
+
+$(document).ready(function() {
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const number1=parseInt($("#subtract1").val());
+		const number2=parseInt($("#subtract2").val());
+		const result = subtract(number1, number2);
+		$("#output1").text(result);
+	});
+});
